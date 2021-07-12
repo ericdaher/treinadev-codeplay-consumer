c3afde132a7b2de9b92c7151dd164aa11d8f99dc
ericdaher <ericdaher2@gmail.com>
2021-07-12 20:55:09 -0300

    API POST with Faraday

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,39 @@
 class Course
-  attr_reader :name, :instructor
-  def initialize(name:, instructor:)
+  attr_reader :name, :instructor, :code, :description, :price, :enrollment_deadline
+  def initialize(name: nil, instructor: nil, code: nil, description: nil, price: nil, enrollment_deadline: nil)
     @name = name
     @instructor = instructor
+    @code = code
+    @description = description
+    @price = price
+    @enrollment_deadline = enrollment_deadline&.to_date
   end
 
   def self.all
     response = Faraday.get 'http://localhost:3000/api/v1/courses'
+    return [] unless response.status == 200
+  
     courses = JSON.parse(response.body, symbolize_names: true)
     courses.map do |course|
-      new(name: course[:name], instructor: course[:instructor])
+      new(name: course[:name], instructor: course[:instructor], code: course[:code])
     end
   rescue Faraday::ConnectionFailed
     []
+  end
+  
+  def self.find(code)
+    response = Faraday.get "http://localhost:3000/api/v1/courses/#{code}"
+    course = JSON.parse(response.body, symbolize_names: true)
+    new(**course.except(:id, :created_at, :updated_at, :instructor_id))
+  end
+
+  def self.create(attributes = {})
+    response = Faraday.post(
+      'http://localhost:3000/api/v1/courses',
+      { course: attributes }.to_json,
+      'Content-Type': 'application/json'
+    )
+    course = JSON.parse(response.body, symbolize_names: true)
+    new(**course.except(:id, :created_at, :updated_at, :instructor_id))
   end
 end
--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe 'Homepage' do
   it 'found codeplay courses' do
     allow(Faraday).to receive(:get).and_return(
-      instance_double(Faraday::Response, status: 200, 
-                                         body: File.read(Rails.root.join('spec/fixtures/courses.json')))
+      instance_double(Faraday::Response, status: 200, body: File.read(Rails.root.join('spec/fixtures/courses.json')))
     )
     
     visit root_path
@@ -14,13 +13,66 @@ describe 'Homepage' do
     expect(page).to have_content('Peter Capaldi')
   end
 
-  it 'found codeplay courses' do
-    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/courses').and_raise(Faraday::ConnectionFailed)
+  it 'could not connect to API' do
+    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/courses').and_raise(Faraday::ConnectionFailed, nil)
 
     visit root_path
 
-    expect(page).to have_content('Ruby')
+    expect(page).to have_content('Site temporariamente indisponível')
+  end
+
+  it 'and API returns 500' do
+    allow(Faraday).to receive(:get).and_return(
+      instance_double(Faraday::Response, status: 500, body: '')
+    )
+
+    visit root_path
+
+    expect(page).to have_content('Site temporariamente indisponível')
+  end
+
+  it 'click on course details' do
+    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/courses').and_return(
+      instance_double(Faraday::Response, status: 200, body: File.read(Rails.root.join('spec/fixtures/courses.json')))
+    )
+    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/courses/RUBYRAILS').and_return(
+      instance_double(Faraday::Response, status: 200, body: File.read(Rails.root.join('spec/fixtures/course.json')))
+    )
+    
+    visit root_path
+    click_on 'Ruby on Rails'
+
     expect(page).to have_content('Ruby on Rails')
-    expect(page).to have_content('Peter Capaldi')
+    expect(page).to have_content('Um curso de Ruby on Rails')
+    expect(page).to have_content('R$ 20,00')
+    expect(page).to have_content('09/10/2021')
+    expect(page).to have_content('RUBYRAILS')
+  end
+
+  it 'creates a course' do
+    allow(Faraday).to receive(:get).and_return(
+      instance_double(Faraday::Response, status: 200, body: '[]')
+    )
+    allow(Faraday).to receive(:post).and_return(
+      instance_double(Faraday::Response, status: 201, body: File.read(Rails.root.join('spec/fixtures/course.json')))
+    )
+
+    visit root_path
+    click_on 'Registrar um novo curso'
+
+    fill_in 'Nome', with: 'Ruby on Rails'
+    fill_in 'Descrição', with: 'Um curso de Ruby on Rails'
+    fill_in 'Preço', with: '20'
+    fill_in 'Código', with: 'RUBYRAILS'
+    fill_in 'Data limite de matrícula', with: '09/10/2021'
+    fill_in 'Instructor ID', with: '1'
+    # select 'Jane Doe', from: 'Instrutor'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Ruby on Rails')
+    expect(page).to have_content('Um curso de Ruby on Rails')
+    expect(page).to have_content('R$ 20,00')
+    expect(page).to have_content('09/10/2021')
+    expect(page).to have_content('RUBYRAILS')
   end
 end
